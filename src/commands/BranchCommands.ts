3ceb3263ad73d6eadfecba8a6e9dc94b6117e938
Emmanuel Gauthier <dev@anorake.net>
2026-03-17 11:23:33 +0100

    feat(start): ask for services scan at the end of branch start

--- src/commands/BranchCommands.ts
+++ src/commands/BranchCommands.ts
@@ -8,6 +8,7 @@ import { checkoutBranch, displayOperationSummary, gitBranchExists, GitOperation,
 import { promptChoice, promptConfirm, promptForBranch } from '../lib/prompt';
 import { confirmSlugOrThrow } from '../lib/slug';
 import { AbstractCommands } from './AbstractCommands';
+import { ServicesCommands } from './ServicesCommands';
 
 export class BranchCommands extends AbstractCommands {
     // ============================================================================
@@ -74,6 +75,14 @@ export class BranchCommands extends AbstractCommands {
 
         // Prepare branch: validate branchName, ensure branch and spec exist
         await (await this.prepareBranch(branchName, baseBranch)).start();
+
+        // Propose to do a service scan to init .envrc file with the right variables for the branch
+        const shouldScan = await promptConfirm(
+            `Do you want to run "forge services scan" now to initialize the .envrc file for this branch?`,
+        );
+        if (shouldScan) {
+            await new ServicesCommands(this.context).scan(branchName);
+        }
 
         // Propose to open the branch in the configured IDE
         if (this.context.ides.length > 0) {
